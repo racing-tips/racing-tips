70b00e0f31414ed65efdedf11e8199a9a8fb312a
Richard Ankers <richardankers@gmail.com>
2018-10-20 15:08:32 +0100

    Add jib for docker build and lombok for java models

---
+++ backend/src/main/java/tips/Tip.java
@@ -0,0 +1,19 @@
+package tips;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Tip {
+    private LocalDateTime eventTime;
+    private String participantName;
+    private BigDecimal odds;
+}
--- backend/src/main/java/tips/TipController.java
+++ backend/src/main/java/tips/TipController.java
@@ -1,15 +1,19 @@
 package tips;
 
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class TipController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @RequestMapping(value = "/tips", method = RequestMethod.GET)
+    public List<Tip> tips() {
+        List<Tip> tipList = new ArrayList<>();
+        tipList.add(new Tip());
+        return tipList;
     }
-
 }
