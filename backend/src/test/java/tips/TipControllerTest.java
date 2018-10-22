024997708f19042f0f1d09427f6e7a16e9bf0b0f
Richard Ankers <richardankers@gmail.com>
2018-10-22 21:54:39 +0100

    Add circle ci

--- backend/src/test/java/tips/TipControllerTest.java
+++ backend/src/test/java/tips/TipControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
